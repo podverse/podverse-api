5755846b8082deac2a52a0246a316851ed22691e
Mitch Downey <mitchldowney@gmail.com>
2018-09-18 00:11:40 -0500

    Allow null on Joi validation fields for update routes

--- src/middleware/validation/update.ts
+++ src/middleware/validation/update.ts
@@ -3,11 +3,11 @@ import { validateBaseBody } from './base'
 
 const validateMediaRefUpdate = async (ctx, next) => {
   const schema = Joi.object().keys({
-    endTime: Joi.number().integer().min(1),
+    endTime: Joi.number().integer().min(1).allow(null),
     id: Joi.string().min(7).max(14).required(),
     isPublic: Joi.boolean(),
     startTime: Joi.number().integer().min(0).required(),
-    title: Joi.string()
+    title: Joi.string().allow(null)
   })
 
   await validateBaseBody(schema, ctx, next)
@@ -15,13 +15,13 @@ const validateMediaRefUpdate = async (ctx, next) => {
 
 const validatePlaylistUpdate = async (ctx, next) => {
   const schema = Joi.object().keys({
-    description: Joi.string(),
+    description: Joi.string().allow(null),
     id: Joi.string().min(7).max(14).required(),
     isPublic: Joi.boolean(),
     itemsOrder: Joi.array().items(Joi.string()),
     mediaRefs: Joi.array().items(Joi.string()),
     ownerId: Joi.string().required(),
-    title: Joi.string()
+    title: Joi.string().allow(null)
   })
 
   await validateBaseBody(schema, ctx, next)
@@ -31,7 +31,7 @@ const validateUserUpdate = async (ctx, next) => {
   const schema = Joi.object().keys({
     email: Joi.string(),
     id: Joi.string().min(7).max(14).required(),
-    name: Joi.string(),
+    name: Joi.string().allow(null),
     ownerId: Joi.string().required(),
     playlists: Joi.array().items(Joi.string()),
     subscribedPodcastIds: Joi.array().items(Joi.string())
