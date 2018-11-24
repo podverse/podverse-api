05e47954b7978c6d091f871c808184953b4c6a66
Mitch Downey <mitchldowney@gmail.com>
2018-11-24 12:48:51 -0600

    Fix broken optionalJwtAuth middleware

--- src/middleware/auth/jwtAuth.ts
+++ src/middleware/auth/jwtAuth.ts
@@ -3,6 +3,8 @@ import * as passport from 'passport'
 export const jwtAuth = passport.authenticate('jwt', { session: false })
 
 export const optionalJwtAuth = async (ctx, next) => {
-  passport.authenticate('jwt', { session: false })
-  await next()
+  await passport.authenticate('jwt', { session: false }, async (_, user) => {
+    ctx.state.user = user
+    await next()
+  })(ctx, next)
 }
