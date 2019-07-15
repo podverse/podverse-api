a2d4ab2e1756ae2e09685ca87a0f9d556f795984
Mitch Downey <mitchldowney@gmail.com>
2019-07-14 22:15:54 -0500

    Update queryUniquePageviews.ts

--- src/scripts/stats/queryUniquePageviews.ts
+++ src/scripts/stats/queryUniquePageviews.ts
@@ -2,8 +2,8 @@ import { queryUniquePageviews } from '~/services/stats'
 
 (async function () {
   try {
-    const pagePath = process.argv.length > 2 ? process.argv[2] : ''
-    const timeRange = process.argv.length > 3 ? process.argv[3] : ''
+    const pagePath = process.argv[2]
+    const timeRange = process.argv[3]
     queryUniquePageviews(pagePath, timeRange)
   } catch (error) {
     console.log(error)
