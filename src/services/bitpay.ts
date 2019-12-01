a3733ec1b817608bb630da00db84b924ef95dfb3
Mitch Downey <mitchldowney@gmail.com>
2019-12-01 06:44:18 -0600

    Update bitpay.ts

--- src/services/bitpay.ts
+++ src/services/bitpay.ts
@@ -6,21 +6,30 @@ const { bitpayConfig } = config
 const { apiKeyPassword, apiKeyPath, currency, notificationURL, price, redirectURL } = bitpayConfig
 const uuidv4 = require('uuid/v4')
 
-  // NOTE: necessary to decrypt your key even if you didn't enter a password
-  // when you generated it. If you did specify a password, pass it as the
-  // first param to bitauth.decrypt()
-const encPrivkey = fs.readFileSync(apiKeyPath).toString()
-const privkey = bitauth.decrypt(apiKeyPassword, encPrivkey)
-const client = bitpay.createClient(privkey)
-console.log('bitpay loading')
-console.log('encPrivKey', encPrivkey)
-console.log('privkey', privkey)
-console.log('client', client)
+// NOTE: It's necessary to decrypt your key even if you didn't enter a password
+// when you generated it. If you did specify a password, pass it as the
+// first param to bitauth.decrypt()
+let encPrivKey
+let privkey
+let client
+try {
+  encPrivKey = fs.readFileSync(apiKeyPath).toString()
+  privkey = bitauth.decrypt(apiKeyPassword, encPrivKey)
+  client = bitpay.createClient(privkey)
+  console.log('bitpay key found')
+  console.log('encPrivKey', encPrivKey)
+  console.log('privkey', privkey)
+  console.log('client', client)
+} catch (error) {
+  console.log('bitpay api.key not found. You\'ll need to setup and pair a BitPay API token within this container.')
+}
 
-client.on('error', err => {
-  console.log('bitpay client.on error')
-  console.log(err)
-})
+if (client) {
+  client.on('error', err => {
+    console.log('bitpay client.on error')
+    console.log(err)
+  })
+}
 
 // Client will take a second to automatically load your tokens,
 // after which it will emit this ready event.
@@ -41,27 +50,31 @@ export const createBitPayInvoiceVendor = email => {
       redirectURL: `${redirectURL}${orderId}`
     }
 
-    client.as('pos').post('invoices', filteredData, (err, invoice) => {
-      if (err) {
-        console.log(err)
-        console.log('createBitPayInvoice client error')
-        reject(err)
-      } else {
-        resolve(invoice)
-      }
-    })
+    if (client) {
+      client.as('pos').post('invoices', filteredData, (err, invoice) => {
+        if (err) {
+          console.log(err)
+          console.log('createBitPayInvoice client error')
+          reject(err)
+        } else {
+          resolve(invoice)
+        }
+      })
+    }
   })
 }
 
 export const getBitPayInvoiceVendor = id => {
   return new Promise((resolve, reject) => {
-    client.get(`invoices/${id}`, function (err, invoice) {
-      if (err) {
-        console.log(err)
-        reject(err)
-      } else {
-        resolve(invoice)
-      }
-    })
+    if (client) {
+      client.get(`invoices/${id}`, function (err, invoice) {
+        if (err) {
+          console.log(err)
+          reject(err)
+        } else {
+          resolve(invoice)
+        }
+      })
+    }
   })
 }
