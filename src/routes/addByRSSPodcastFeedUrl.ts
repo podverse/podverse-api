d4092597426133fd9645cd7d4e20d428cb8596c9
Mitch Downey <mitchldowney@gmail.com>
2020-05-17 15:29:33 -0500

    Update addByRSSPodcastFeedUrl.ts

--- src/routes/addByRSSPodcastFeedUrl.ts
+++ src/routes/addByRSSPodcastFeedUrl.ts
@@ -1,7 +1,7 @@
 import * as bodyParser from 'koa-bodyparser'
 import * as Router from 'koa-router'
 import { config } from '~/config'
-import { toggleSubscribeToAddByRSSPodcastFeedUrl } from '~/controllers/user'
+import { addByRSSPodcastFeedUrlAdd, addByRSSPodcastFeedUrlRemove } from '~/controllers/user'
 import { emitRouterError } from '~/lib/errors'
 import { jwtAuth } from '~/middleware/auth/jwtAuth'
 import { hasValidMembership } from '~/middleware/hasValidMembership'
@@ -13,16 +13,15 @@ const router = new Router({ prefix: `${config.apiPrefix}${config.apiVersion}/add
 
 router.use(bodyParser())
 
-// Toggle subscribe to addByRSSPodcastFeedUrl
-const toggleSubscribeLimiter = RateLimit.middleware({
+const addLimiter = RateLimit.middleware({
   interval: 1 * 60 * 1000,
   max: rateLimiterMaxOverride || 15,
   message: `You're doing that too much. Please try again in a minute.`,
-  prefixKey: 'post/add-by-rss-podcast-feed-url/toggle-subscribe'
+  prefixKey: 'post/add-by-rss-podcast-feed-url/add'
 })
 
-router.post('/toggle-subscribe',
-  toggleSubscribeLimiter,
+router.post('/add',
+  addLimiter,
   jwtAuth,
   hasValidMembership,
   async ctx => {
@@ -36,12 +35,42 @@ router.post('/toggle-subscribe',
         ctx.body = { message: 'Invalid addByRSSPodcastFeedUrl. Please check your URL.' }
         ctx.status = 400
       } else {
-        const addByRSSPodcastFeedUrls = await toggleSubscribeToAddByRSSPodcastFeedUrl(addByRSSPodcastFeedUrl, ctx.state.user.id)
+        const addByRSSPodcastFeedUrls = await addByRSSPodcastFeedUrlAdd(addByRSSPodcastFeedUrl, ctx.state.user.id)
         ctx.body = addByRSSPodcastFeedUrls
       }
     } catch (error) {
       emitRouterError(error, ctx)
     }
   })
+
+const removeLimiter = RateLimit.middleware({
+  interval: 1 * 60 * 1000,
+  max: rateLimiterMaxOverride || 15,
+  message: `You're doing that too much. Please try again in a minute.`,
+  prefixKey: 'post/add-by-rss-podcast-feed-url/remove'
+})
+
+router.post('/remove',
+  removeLimiter,
+  jwtAuth,
+  hasValidMembership,
+  async ctx => {
+    try {
+      const { addByRSSPodcastFeedUrl } = ctx.request.body as any
+
+      if (!addByRSSPodcastFeedUrl) {
+        ctx.body = { message: 'An addByRSSPodcastFeedUrl field is required in the request body.' }
+        ctx.status = 400
+      } else if (!validUrl.isUri(addByRSSPodcastFeedUrl)) {
+        ctx.body = { message: 'Invalid addByRSSPodcastFeedUrl. Please check your URL.' }
+        ctx.status = 400
+      } else {
+        const addByRSSPodcastFeedUrls = await addByRSSPodcastFeedUrlRemove(addByRSSPodcastFeedUrl, ctx.state.user.id)
+        ctx.body = addByRSSPodcastFeedUrls
+      }
+    } catch (error) {
+      emitRouterError(error, ctx)
+    }
+  })  
 
 export const addByRSSPodcastFeedUrlRouter = router
