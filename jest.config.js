9891e76da973623b75dc95f1e7d5711b5cfb2adf
Mitch Downey <mitchldowney@gmail.com>
2020-07-05 04:21:41 -0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   globals: {
-    // we must specify a custom tsconfig for tests because we need the typescript transform
-    // to transform jsx into js rather than leaving it jsx such as the next build requires.  you
-    // can see this setting in tsconfig.jest.json -> "jsx": "react"
     "ts-jest": {
       tsConfig: "tsconfig.json"
     }
