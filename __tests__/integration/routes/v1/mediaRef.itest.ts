d86ccdd6cfa493d85f19d16d39e58865a80d16a6
Kyle Downey <capntechbeard@gmail.com>
2020-08-24 18:38:26 -0500

    Cont. integration tests

--- __tests__/integration/routes/v1/mediaRef.itest.ts
+++ __tests__/integration/routes/v1/mediaRef.itest.ts
@@ -1,6 +1,6 @@
 import * as chai from 'chai'
 import chaiHttp = require('chai-http')
-import { v1Path } from '../../utils'
+import { testUsers, v1Path } from '../../utils'
 const { expect: chaiExpect } = chai
 chai.use(chaiHttp)
 
@@ -12,47 +12,47 @@ describe('_mediaRef endpoints', () => {
         .get(`${v1Path}/mediaRef/9rA5BhWp`)
         .end((err, res) => {
           chaiExpect(res).to.have.status(200);
-          chaiExpect(res.body.id).to.eql('9rA5BhWp')
-          chaiExpect(res.body.endTime).to.eql(1680)
-          chaiExpect(res.body.isPublic).to.eql(true)
-          chaiExpect(res.body.pastHourTotalUniquePageviews).to.eql(7)
-          chaiExpect(res.body.pastDayTotalUniquePageviews).to.eql(8)
-          chaiExpect(res.body.pastWeekTotalUniquePageviews).to.eql(9)
-          chaiExpect(res.body.pastMonthTotalUniquePageviews).to.eql(0)
-          chaiExpect(res.body.pastYearTotalUniquePageviews).to.eql(1)
-          chaiExpect(res.body.pastAllTimeTotalUniquePageviews).to.eql(2)
-          chaiExpect(res.body.startTime).to.eql(1500)
-          chaiExpect(res.body.title).to.eql('Amet aliquam id diam maecenas ultricies mi eget.')
-          chaiExpect(res.body.createdAt).to.eql('2020-03-02T22:37:36.073Z')
-          chaiExpect(res.body.updatedAt).to.eql('2020-03-02T22:58:19.378Z')
-          //authors
-          //categories
+          chaiExpect(res.body.id).to.equal('9rA5BhWp')
+          chaiExpect(res.body.endTime).to.equal(1680)
+          chaiExpect(res.body.isPublic).to.equal(true)
+          chaiExpect(res.body.pastHourTotalUniquePageviews).to.equal(7)
+          chaiExpect(res.body.pastDayTotalUniquePageviews).to.equal(8)
+          chaiExpect(res.body.pastWeekTotalUniquePageviews).to.equal(9)
+          chaiExpect(res.body.pastMonthTotalUniquePageviews).to.equal(0)
+          chaiExpect(res.body.pastYearTotalUniquePageviews).to.equal(1)
+          chaiExpect(res.body.pastAllTimeTotalUniquePageviews).to.equal(2)
+          chaiExpect(res.body.startTime).to.equal(1500)
+          chaiExpect(res.body.title).to.equal('Amet aliquam id diam maecenas ultricies mi eget.')
+          chaiExpect(res.body.createdAt).to.equal('2020-03-02T22:37:36.073Z')
+          chaiExpect(res.body.updatedAt).to.equal('2020-03-02T22:58:19.378Z')
+          chaiExpect(res.body.authors).to.eql([])
+          chaiExpect(res.body.categories).to.eql([])
           
 
           const episode = res.body.episode
-          chaiExpect(episode.id).to.eql('fFmGXkgIM')
+          chaiExpect(episode.id).to.equal('fFmGXkgIM')
           chaiExpect(episode).to.have.property('description')
-          chaiExpect(episode.duration).to.eql(0)
-          chaiExpect(episode.episodeType).to.eql('full')
-          chaiExpect(episode.guid).to.eql('465b2bdc-eebd-11e9-85c8-171e42a72b35')
-          chaiExpect(episode.imageUrl).to.eql(null)
-          chaiExpect(episode.isExplicit).to.eql(false)
-          chaiExpect(episode.isPublic).to.eql(true)
-          chaiExpect(episode.linkUrl).to.eql(null)
-          chaiExpect(episode.mediaFilesize).to.eql(0)
-          chaiExpect(episode.mediaType).to.eql('audio/mpeg')
-          chaiExpect(episode.mediaUrl).to.eql('https://www.podtrac.com/pts/redirect.mp3/pdst.fm/e/chtbl.com/track/524GE/traffic.megaphone.fm/VMP3689667624.mp3')
-          chaiExpect(episode.pastHourTotalUniquePageviews).to.eql(1)
-          chaiExpect(episode.pastDayTotalUniquePageviews).to.eql(2)
-          chaiExpect(episode.pastWeekTotalUniquePageviews).to.eql(3)
-          chaiExpect(episode.pastMonthTotalUniquePageviews).to.eql(4)
-          chaiExpect(episode.pastYearTotalUniquePageviews).to.eql(5)
-          chaiExpect(episode.pastAllTimeTotalUniquePageviews).to.eql(6)
-          chaiExpect(episode.pubDate).to.eql('2020-03-02T05:01:00.000Z')
-          chaiExpect(episode.title).to.eql('Jason Calacanis: TikTok should be banned, Tim Cook doesn\'t have enough \"chutzpah,\" and Uber will be fine')
-          chaiExpect(episode.podcastId).to.eql('zRo1jwx67')
-          chaiExpect(episode.createdAt).to.eql('2020-03-02T21:17:39.462Z')
-          chaiExpect(episode.updatedAt).to.eql('2020-04-03T06:52:52.361Z')
+          chaiExpect(episode.duration).to.equal(0)
+          chaiExpect(episode.episodeType).to.equal('full')
+          chaiExpect(episode.guid).to.equal('465b2bdc-eebd-11e9-85c8-171e42a72b35')
+          chaiExpect(episode.imageUrl).to.equal(null)
+          chaiExpect(episode.isExplicit).to.equal(false)
+          chaiExpect(episode.isPublic).to.equal(true)
+          chaiExpect(episode.linkUrl).to.equal(null)
+          chaiExpect(episode.mediaFilesize).to.equal(0)
+          chaiExpect(episode.mediaType).to.equal('audio/mpeg')
+          chaiExpect(episode.mediaUrl).to.equal('https://www.podtrac.com/pts/redirect.mp3/pdst.fm/e/chtbl.com/track/524GE/traffic.megaphone.fm/VMP3689667624.mp3')
+          chaiExpect(episode.pastHourTotalUniquePageviews).to.equal(1)
+          chaiExpect(episode.pastDayTotalUniquePageviews).to.equal(2)
+          chaiExpect(episode.pastWeekTotalUniquePageviews).to.equal(3)
+          chaiExpect(episode.pastMonthTotalUniquePageviews).to.equal(4)
+          chaiExpect(episode.pastYearTotalUniquePageviews).to.equal(5)
+          chaiExpect(episode.pastAllTimeTotalUniquePageviews).to.equal(6)
+          chaiExpect(episode.pubDate).to.equal('2020-03-02T05:01:00.000Z')
+          chaiExpect(episode.title).to.equal('Jason Calacanis: TikTok should be banned, Tim Cook doesn\'t have enough \"chutzpah,\" and Uber will be fine')
+          chaiExpect(episode.podcastId).to.equal('zRo1jwx67')
+          chaiExpect(episode.createdAt).to.equal('2020-03-02T21:17:39.462Z')
+          chaiExpect(episode.updatedAt).to.equal('2020-04-03T06:52:52.361Z')
 
           
           
@@ -66,7 +66,59 @@ describe('_mediaRef endpoints', () => {
         .get(`${v1Path}/mediaRef/9rA5BasdfhWp`)
         .end((err, res) => {
           chaiExpect(res).to.have.status(404);
-          chaiExpect(res.body.message).to.eql('MediaRef not found')
+          chaiExpect(res.body.message).to.equal('MediaRef not found')
+
+          done()
+        })
+    })
+  })
+  describe('mediaRef create', () => {
+    const sendBody = {
+      "authors": [],
+      "categories": [],
+      "endTime": 100,
+      "episodeId": "gRgjd3YcKb",
+      "isPublic": "true",
+      "startTime": 50,
+      "title": "Sample clip title"
+    }
+
+    test('when the user is not logged in', async (done) => {
+      chai.request(global.app)
+        .post(`${v1Path}/mediaRef`)
+        .send(sendBody)
+        .end((err, res) => {
+          chaiExpect(res).to.have.status(401)
+
+          done()
+        })
+    })
+
+    test('when the user is logged in', async (done) => {
+      chai.request(global.app)
+        .post(`${v1Path}/mediaRef`)
+        .set('Cookie', testUsers.premium.authCookie)
+        .send(sendBody)
+        .end((err, res) => {
+          chaiExpect(res).to.have.status(200)
+
+          chaiExpect(res.body.authors).to.eql([])
+          chaiExpect(res.body.categories).to.eql([])
+          chaiExpect(res.body.endTime).to.equal(100)
+          chaiExpect(res.body.isPublic).to.equal(false)
+          chaiExpect(res.body.startTime).to.equal(50)
+          chaiExpect(res.body.title).to.equal('Sample clip title')
+          chaiExpect(res.body.owner).to.equal('QMReJmbE')
+          chaiExpect(res.body.episode).to.equal('gRgjd3YcKb')
+          chaiExpect(res.body).to.have.property('id')
+          chaiExpect(res.body.pastHourTotalUniquePageviews).to.equal(0)
+          chaiExpect(res.body.pastDayTotalUniquePageviews).to.equal(0)
+          chaiExpect(res.body.pastWeekTotalUniquePageviews).to.equal(0)
+          chaiExpect(res.body.pastMonthTotalUniquePageviews).to.equal(0)
+          chaiExpect(res.body.pastYearTotalUniquePageviews).to.equal(0)
+          chaiExpect(res.body.pastAllTimeTotalUniquePageviews).to.equal(0)
+          chaiExpect(res.body).to.have.property('createdAt')
+          chaiExpect(res.body).to.have.property('updatedAt')
 
           done()
         })
