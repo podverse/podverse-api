3a19967ee0465d9a754517f066d5c889963b4f10
Mitch Downey <mitchldowney@gmail.com>
2020-09-27 18:18:06 -0500

    Replace react-promise-native with axios

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -1,16 +1,17 @@
-import * as requestPromiseNative from 'request-promise-native'
+import axios from 'axios'
 import { config } from '~/config'
 const { userAgent } = config
 
 export const request = async (url: string, options?: any) => {
   const headers = (options && options.headers) || {}
-  const response = await requestPromiseNative(url, {
-    ...options,
+  const response = await axios({
     timeout: 10000,
+    url,
+    ...options,
     headers: {
       ...headers,
       'User-Agent': userAgent
-    },
+    }
   })
 
   return response
