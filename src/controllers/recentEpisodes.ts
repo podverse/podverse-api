b85b2c5c38fcc31cc0246e4f2c3b8674ce4c0de2
Mitch Downey <mitchldowney@gmail.com>
2020-11-22 16:47:04 -0600

    Update recentEpisodes.ts

--- src/controllers/recentEpisodes.ts
+++ src/controllers/recentEpisodes.ts
@@ -1,7 +1,7 @@
 import { getConnection } from 'typeorm'
 
 const days = process.env.RECENT_EPISODES_DATE_RANGE ?
-  parseInt(process.env.RECENT_EPISODES_DATE_RANGE) : 21
+  parseInt(process.env.RECENT_EPISODES_DATE_RANGE) : 14
 
 const updateRecentEpisodesTables = async () => {
   const promises = [] as any
