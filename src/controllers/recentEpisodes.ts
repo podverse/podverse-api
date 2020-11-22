c9d6d04ba3b07e8eea3c6b3c01a7df23c062557e
Mitch Downey <mitchldowney@gmail.com>
2020-11-21 22:55:52 -0600

    Run updateRecentEpisodesTables job in chunks

--- src/controllers/recentEpisodes.ts
+++ src/controllers/recentEpisodes.ts
@@ -1,28 +1,29 @@
 import { getConnection } from 'typeorm'
 
+const days = process.env.RECENT_EPISODES_DATE_RANGE ?
+  parseInt(process.env.RECENT_EPISODES_DATE_RANGE) : 21
+
 const updateRecentEpisodesTables = async () => {
-  const date = new Date()
-  date.setDate(date.getDate() - 14)
-  const dateString = date.toISOString().slice(0, 19).replace('T', ' ')
+  const promises = [] as any
   const em = await getConnection().createEntityManager()
 
-// Create recentEpisodesByCategory table
+  await dropTempTables(em)
 
-  await em.query(`
-    CREATE TABLE "recentEpisodesByCategoryTemp" (
-      LIKE "recentEpisodesByCategory" INCLUDING ALL
-    );
-  `)
+  for (let i = 0; i < days; i++) {
+    const byCategoryPromise = generateByCategoryQueryPromise(em, i)
+    const byPodcastPromise = generateByPodcastQueryPromise(em, i)
+    promises.push(byCategoryPromise)
+    promises.push(byPodcastPromise)
+  }
 
-  await em.query(`
-    INSERT INTO "recentEpisodesByCategoryTemp" ("categoryId", "episodeId", "pubDate")
-    SELECT COALESCE(podcasts_categories_categories."categoriesId", '0'),
-    episodes.id as episodeId, episodes."pubDate" as pubDate
-    FROM episodes
-    LEFT JOIN podcasts_categories_categories
-    ON podcasts_categories_categories."podcastsId" = episodes."podcastId"
-    WHERE episodes."isPublic" = TRUE AND episodes."pubDate" > '${dateString}'
-  `)
+  await Promise.all(promises)
+
+  let byCategorySelects = ''
+  for (let i = 0; i < days; i++) {
+    byCategorySelects += `SELECT * FROM "recentEpisodesByCategoryTemp${i}" ${i === days - 1 ? '' : 'UNION '}`
+  }
+
+  await em.query(`CREATE TABLE "recentEpisodesByCategoryTempCombined" AS (${byCategorySelects});`)
 
   await em.query(`
     ALTER TABLE "recentEpisodesByCategory"
@@ -30,26 +31,18 @@ const updateRecentEpisodesTables = async () => {
   `)
 
   await em.query(`
-    ALTER TABLE "recentEpisodesByCategoryTemp"
+    ALTER TABLE "recentEpisodesByCategoryTempCombined"
     RENAME TO "recentEpisodesByCategory";
   `)
 
   await em.query(`DROP TABLE "recentEpisodesByCategoryOld" CASCADE;`)
 
-  // Create recentEpisodesByPodcast table
+  let byPodcastSelects = ''
+  for (let i = 0; i < days; i++) {
+    byPodcastSelects += `SELECT * FROM "recentEpisodesByPodcastTemp${i}" ${i === days - 1 ? '' : 'UNION '}`
+  }
 
-  await em.query(`
-    CREATE TABLE "recentEpisodesByPodcastTemp" (
-      LIKE "recentEpisodesByPodcast" INCLUDING ALL
-    );
-  `)
-
-  await em.query(`
-    INSERT INTO "recentEpisodesByPodcastTemp" ("podcastId", "episodeId", "pubDate")
-    SELECT episodes."podcastId", episodes.id as episodeId, episodes."pubDate" as pubDate
-    FROM episodes
-    WHERE episodes."isPublic" = TRUE AND episodes."pubDate" > '${dateString}'
-  `)
+  await em.query(`CREATE TABLE "recentEpisodesByPodcastTempCombined" AS (${byPodcastSelects});`)
 
   await em.query(`
     ALTER TABLE "recentEpisodesByPodcast"
@@ -57,11 +50,80 @@ const updateRecentEpisodesTables = async () => {
   `)
 
   await em.query(`
-    ALTER TABLE "recentEpisodesByPodcastTemp"
+    ALTER TABLE "recentEpisodesByPodcastTempCombined"
     RENAME TO "recentEpisodesByPodcast";
   `)
 
   await em.query(`DROP TABLE "recentEpisodesByPodcastOld" CASCADE;`)
+
+  await dropTempTables(em)
+}
+
+const generateByCategoryQueryPromise = (em, i) => {
+  return new Promise(async resolve => {
+    await em.query(`
+      CREATE TABLE "recentEpisodesByCategoryTemp${i}" (
+        LIKE "recentEpisodesByCategory" INCLUDING ALL
+      );
+    `)
+    
+    await em.query(`
+      INSERT INTO "recentEpisodesByCategoryTemp${i}" ("categoryId", "episodeId", "pubDate")
+      SELECT COALESCE(podcasts_categories_categories."categoriesId", '0'),
+      episodes.id as episodeId, episodes."pubDate" as pubDate
+      FROM episodes
+      LEFT JOIN podcasts_categories_categories
+      ON podcasts_categories_categories."podcastsId" = episodes."podcastId"
+      WHERE episodes."isPublic" = TRUE
+      AND episodes."pubDate" > '${getDateString(i)}'
+      AND episodes."pubDate" <= '${getDateString(i, true)}'
+    `)
+    
+    resolve()
+  })
+}
+
+const generateByPodcastQueryPromise = (em, i) => {
+  return new Promise(async resolve => {
+    await em.query(`
+      CREATE TABLE "recentEpisodesByPodcastTemp${i}" (
+        LIKE "recentEpisodesByPodcast" INCLUDING ALL
+      );
+    `)
+
+    await em.query(`
+      INSERT INTO "recentEpisodesByPodcastTemp${i}" ("podcastId", "episodeId", "pubDate")
+      SELECT episodes."podcastId", episodes.id as episodeId, episodes."pubDate" as pubDate
+      FROM episodes
+      WHERE episodes."isPublic" = TRUE
+      AND episodes."pubDate" > '${getDateString(i)}'
+      AND episodes."pubDate" <= '${getDateString(i, true)}'
+    `)
+
+    resolve()
+  })
+}
+
+const dropTempTables = async (em) => {
+  for (let i = 0; i < days; i++) {
+    await em.query(`DROP TABLE IF EXISTS "recentEpisodesByCategoryTemp${i}" CASCADE;`)
+  }
+
+  for (let i = 0; i < days; i++) {
+    await em.query(`DROP TABLE IF EXISTS "recentEpisodesByPodcastTemp${i}" CASCADE;`)
+  }
+
+  await em.query('DROP TABLE IF EXISTS "recentEpisodesByCategoryOld";')
+  await em.query('DROP TABLE IF EXISTS "recentEpisodesByCategoryTempCombined";')
+  await em.query('DROP TABLE IF EXISTS "recentEpisodesByPodcastOld";')
+  await em.query('DROP TABLE IF EXISTS "recentEpisodesByPodcastTempCombined";')
+}
+
+const getDateString = (i, isEndDate = false) => {
+  const date = new Date()
+  i = isEndDate ? i + 1 : i
+  date.setDate(date.getDate() - 14 + i)
+  return date.toISOString().slice(0, 19).replace('T', ' ')
 }
 
 export {
