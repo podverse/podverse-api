425da84497646a80a941e6cdd450844d16b8c96e
Mitch Downey <mitchldowney@gmail.com>
2020-11-23 19:31:38 -0600

    Increase parser request timeout to 30 seconds

--- src/services/parser.ts
+++ src/services/parser.ts
@@ -19,7 +19,7 @@ export const parseFeedUrl = async (feedUrl, forceReparsing = false) => {
     const result = await podcastFeedParser.getPodcastFromURL({
       url: feedUrl.url,
       headers: { 'User-Agent': userAgent },
-      timeout: 10000
+      timeout: 30000
     })
     const { episodes, meta } = result
 
