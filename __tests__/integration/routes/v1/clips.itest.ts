edebf2559ff4d5e4aaa0990a5e26f98313946c31
KyleFDowney <capntechbeard@gmail.com>
2020-12-07 14:46:00 -0600

    cont. itests: clips + server-test

--- __tests__/integration/routes/v1/clips.itest.ts
+++ __tests__/integration/routes/v1/clips.itest.ts
@@ -13,38 +13,38 @@ describe('_author endpoints', () => {
         .end((err, res) => {
           chaiExpect(res).to.have.status(200);
           
-          // chaiExpect(res.body.version).to.equal('1.0.0')
+          chaiExpect(res.body.version).to.equal('1.0.0')
 
-          // const chapter0 = res.body.chapters[0]
+          const chapter0 = res.body.chapters[0]
 
-          // chaiExpect(chapter0.endTime).to.equal(30)
-          // chaiExpect(chapter0.startTime).to.equal(0)
-          // chaiExpect(chapter0.title).to.equal('First soundbite')
+          chaiExpect(chapter0.endTime).to.equal(30)
+          chaiExpect(chapter0.startTime).to.equal(0)
+          chaiExpect(chapter0.title).to.equal('First soundbite')
 
-          // const chapter1 = res.body.chapters[0]
+          const chapter1 = res.body.chapters[1]
 
-          // chaiExpect(chapter1.endTime).to.equal(207)
-          // chaiExpect(chapter1.startTime).to.equal(122)
-          // chaiExpect(chapter1.title).to.equal('Some soundbite title')
+          chaiExpect(chapter1.endTime).to.equal(207)
+          chaiExpect(chapter1.startTime).to.equal(122)
+          chaiExpect(chapter1.title).to.equal('Some soundbite title')
 
-          // const chapter2 = res.body.chapters[0]
+          const chapter2 = res.body.chapters[2]
 
-          // chaiExpect(chapter2.endTime).to.equal(360)
-          // chaiExpect(chapter2.startTime).to.equal(240)
-          // chaiExpect(chapter2.title).to.equal('One more soundbite title')
+          chaiExpect(chapter2.endTime).to.equal(360)
+          chaiExpect(chapter2.startTime).to.equal(240)
+          chaiExpect(chapter2.title).to.equal('Another soundbite title')
 
-          // const chapter3 = res.body.chapters[0]
+          const chapter3 = res.body.chapters[3]
 
-          // chaiExpect(chapter3.endTime).to.equal(632)
-          // chaiExpect(chapter3.startTime).to.equal(482)
-          // chaiExpect(chapter3.title).to.equal('Another soundbite title')
+          chaiExpect(chapter3.endTime).to.equal(632)
+          chaiExpect(chapter3.startTime).to.equal(482)
+          chaiExpect(chapter3.title).to.equal('One more soundbite title')
 
-          // const chapter4 = res.body.chapters[0]
+          const chapter4 = res.body.chapters[4]
 
-          // chaiExpect(chapter4.endTime).to.equal(735)
-          // chaiExpect(chapter4.startTime).to.equal(555)
+          chaiExpect(chapter4.endTime).to.equal(735)
+          chaiExpect(chapter4.startTime).to.equal(555)
 
-          // chaiExpect(Object.keys(res.body).length).to.equal(5)
+          chaiExpect(Object.keys(res.body).length).to.equal(2)
                 
           done()
         })
