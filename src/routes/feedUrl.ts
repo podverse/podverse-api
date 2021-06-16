c96c4f6ced67a9c58490e533b1c1dd37e4bc68eb
Mitch Downey <mitchldowney@gmail.com>
2021-06-16 13:14:27 -0500

    Remove getFeedUrls endpoint

--- src/routes/feedUrl.ts
+++ src/routes/feedUrl.ts
@@ -1,25 +1,9 @@
 import * as Router from 'koa-router'
 import { config } from '~/config'
 import { emitRouterError } from '~/lib/errors'
-import { getFeedUrl, getFeedUrls } from '~/controllers/feedUrl'
-import { parseQueryPageOptions } from '~/middleware/parseQueryPageOptions'
-import { validateFeedUrlSearch } from '~/middleware/queryValidation/search'
+import { getFeedUrl } from '~/controllers/feedUrl'
 
 const router = new Router({ prefix: `${config.apiPrefix}${config.apiVersion}/feedUrl` })
-
-// Search
-router.get('/',
-  (ctx, next) => parseQueryPageOptions(ctx, next, 'feedUrls'),
-  validateFeedUrlSearch,
-  async ctx => {
-    try {
-      const feedUrls = await getFeedUrls(ctx.state.query)
-      ctx.body = feedUrls
-    } catch (error) {
-      emitRouterError(error, ctx)
-    }
-  }
-)
 
 // Get
 router.get('/:id',
