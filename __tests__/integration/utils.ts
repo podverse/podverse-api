76aea21d4d4dfb4916053499b1685c8c8b774c9c
Mitch Downey <mitchldowney@gmail.com>
2021-09-16 14:00:38 -0500

    Update test Bearer tokens to expire in 100 years

--- __tests__/integration/utils.ts
+++ __tests__/integration/utils.ts
@@ -2,16 +2,16 @@ export const v1Path = '/api/v1'
 
 export const testUsers = {
   freeTrial: {
-    authCookie: 'Authorization=Bearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkVWSERCUlpZIiwiZXhwIjoxNjI5NTE5NjM0LjQxMSwiaWF0IjoxNTk3OTgzNjM0fQ.5urqOHN-rrKC91fChGFZZqjVgCM3koJVWa-acGcYDXI'
+    authCookie: 'Authorization=Bearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkVWSERCUlpZIiwiZXhwIjo0Nzg1NDE4NzA4LjYzMSwiaWF0IjoxNjMxODE4NzA4fQ._-qWyLGNoqRawrJal0uQi5u-Oay0UEa43PZOKqL94ys'
   },
   freeTrialExpired: {
-    authCookie: 'Authorization=Bearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImJ2VmpzUUNIIiwiZXhwIjoxNjI5NTE5NjQ4LjA3NSwiaWF0IjoxNTk3OTgzNjQ4fQ.INjkM-oI4V2OCQhZH76eumQrHSyku2352t3rnc6o2ZE'
+    authCookie: 'Authorization=Bearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImJ2VmpzUUNIIiwiZXhwIjo0Nzg1NDE4NzM4LjEzOCwiaWF0IjoxNjMxODE4NzM4fQ.4b_xpW-3iMOK1TaD9xdjNO3mXQP3h9B7Qycy-JLcukg'
   },
   premium: {
-    authCookie: 'Authorization=Bearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlFNUmVKbWJFIiwiZXhwIjoxNjI5NTE4MjIxLjYwMywiaWF0IjoxNTk3OTgyMjIxfQ.BXiXGynXbvJyubx_t9n2uHxV8evuE_bzEy9yahi-wdI'
+    authCookie: 'Authorization=Bearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlFNUmVKbWJFIiwiZXhwIjo0Nzg1NDE4NzY1LjQ5NSwiaWF0IjoxNjMxODE4NzY1fQ.F4Cmbz7B9lLG1O_t9rB6H7gMUQaP0qQ8Y2_zJp_9-LM'
   },
   premiumExpired: {
-    authCookie: 'Authorization=Bearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Im9BYlBQUkY5IiwiZXhwIjoxNjI5NTE5NjA0LjkwMSwiaWF0IjoxNTk3OTgzNjA0fQ.2RmV5Py8cUR-ckkqus262fMv-3MJNxM1EmIRLun0k3o'
+    authCookie: 'Authorization=Bearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Im9BYlBQUkY5IiwiZXhwIjo0Nzg1NDE4NzQ5LjAxMSwiaWF0IjoxNjMxODE4NzQ5fQ.QnH9KCQVX_Bwd7-jdGW1XP0GSdbshSfqmNRiJ6IHRyk'
   }
 }
 
