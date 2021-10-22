119585492670d3834d5fed58efd5755e80d8d150
Mitch Downey <mitchldowney@gmail.com>
2021-10-22 01:07:50 -0500

    Update 0018_mediaRef_index_isOfficialSoundBite_isOfficialChapter.sql

--- migrations/0018_mediaRef_index_isOfficialSoundBite_isOfficialChapter.sql
+++ migrations/0018_mediaRef_index_isOfficialSoundBite_isOfficialChapter.sql
@@ -1,2 +1,3 @@
-CREATE INDEX index_mediaRefs_isOfficialChapter ON mediaRefs ("isOfficialChapter");
-CREATE INDEX index_mediaRefs_isOfficialSoundBite ON mediaRefs ("isOfficialSoundBite");
+CREATE INDEX index_mediaRefs_isOfficialChapter ON "mediaRefs" ("isOfficialChapter");
+CREATE INDEX index_mediaRefs_isOfficialSoundBite ON "mediaRefs" ("isOfficialSoundBite");
+CREATE INDEX CONCURRENTLY "index_mediaRefs_createdAt" ON "mediaRefs" ("createdAt" DESC);
