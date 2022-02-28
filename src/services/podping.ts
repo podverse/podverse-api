b69d4b5c80a4721155ff64b1004d8e0a29ec91a8
Mitch Downey <mitchldowney@gmail.com>
2022-02-27 22:08:19 -0600

    Handle podping websocket close and error events

--- src/services/podping.ts
+++ src/services/podping.ts
@@ -1,6 +1,6 @@
-import * as WebSocket from 'ws'
 import { getFeedUrlByUrl } from '~/controllers/feedUrl'
 import { addFeedUrlsByPodcastIndexId } from './queue'
+const ws = require('ws')
 
 export const runLiveItemListener = () => {
   console.log('starting runLiveItemListener')
@@ -13,34 +13,78 @@ export const runLiveItemListener = () => {
     console.log('runLiveItemListener interval')
   }, 100000000)
 
-  const ws = new WebSocket('wss://api.livewire.io/ws/podping')
+  let openedSocket: boolean | null = null
+  const timeInterval = 5000
+  const port = 3000
+  const url = 'wss://api.livewire.io/ws/podping'
 
-  ws.on('message', async function message(data) {
-    try {
-      const msg = JSON.parse(data)
+  function connect() {
+    const client = new ws(url)
+    return new Promise((resolve, reject) => {
+      console.log('client try to connect...')
 
-      if (msg.t === 'podping') {
-        for (const p of msg.p) {
-          if (p.p.reason === 'live') {
-            const podcastIndexIds: string[] = []
-            for (const url of p.p.iris) {
-              try {
-                if (url?.startsWith('http')) {
-                  const feedUrl = await getFeedUrlByUrl(url)
-                  const { podcastIndexId } = feedUrl.podcast
-                  if (podcastIndexId) podcastIndexIds.push(podcastIndexId)
+      client.on('open', () => {
+        console.log('WEBSOCKET_OPEN: client connected to server at port %s', port)
+        openedSocket = true
+        resolve(openedSocket)
+      })
+
+      client.on('message', async function message(data) {
+        try {
+          const msg = JSON.parse(data)
+          if (msg.t === 'podping') {
+            for (const p of msg.p) {
+              if (p.p.reason === 'live') {
+                const podcastIndexIds: string[] = []
+                for (const url of p.p.iris) {
+                  try {
+                    if (url?.startsWith('http')) {
+                      const feedUrl = await getFeedUrlByUrl(url)
+                      const { podcastIndexId } = feedUrl.podcast
+                      if (podcastIndexId) podcastIndexIds.push(podcastIndexId)
+                    }
+                  } catch (err) {
+                    console.log('p.p.iris error', err)
+                  }
                 }
-              } catch (err) {
-                console.log('p.p.iris error', err)
+                const queueType = 'live'
+                await addFeedUrlsByPodcastIndexId(podcastIndexIds, queueType)
               }
             }
-            const queueType = 'live'
-            await addFeedUrlsByPodcastIndexId(podcastIndexIds, queueType)
           }
+        } catch (err) {
+          console.log('message error:', err)
         }
-      }
+      })
+
+      client.on('close', (err) => {
+        console.log('WEBSOCKET_CLOSE: connection closed %o', err)
+        openedSocket = false
+        reject(err)
+      })
+
+      client.on('error', (err) => {
+        console.log('WEBSOCKET_ERROR: Error', new Error(err.message))
+        openedSocket = false
+        reject(err)
+      })
+    })
+  }
+
+  async function reconnect() {
+    try {
+      await connect()
     } catch (err) {
-      console.log('message error:', err)
+      console.log('WEBSOCKET_RECONNECT: Error', new Error(err).message)
     }
-  })
+  }
+
+  reconnect()
+
+  // repeat every 5 seconds
+  setInterval(() => {
+    if (!openedSocket) {
+      reconnect()
+    }
+  }, timeInterval)
 }
