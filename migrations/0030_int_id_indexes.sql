ff6160ab93c480b59610552865bb80cf7a9c023e
Mitch Downey <mitchldowney@gmail.com>
2022-03-18 16:57:18 -0500

    Create 0030_int_id_indexes.sql

---
+++ migrations/0030_int_id_indexes.sql
@@ -0,0 +1,7 @@
+CREATE INDEX CONCURRENTLY "authors_int_id_index" ON "authors" ("int_id");
+CREATE INDEX CONCURRENTLY "categories_int_id_index" ON "categories" ("int_id");
+CREATE INDEX CONCURRENTLY "feedUrls_int_id_index" ON "feedUrls" ("int_id");
+CREATE INDEX CONCURRENTLY "mediaRefs_int_id_index" ON "mediaRefs" ("int_id");
+CREATE INDEX CONCURRENTLY "playlists_int_id_index" ON "playlists" ("int_id");
+CREATE INDEX CONCURRENTLY "podcasts_int_id_index" ON "podcasts" ("int_id");
+CREATE INDEX CONCURRENTLY "users_int_id_index" ON "users" ("int_id");
