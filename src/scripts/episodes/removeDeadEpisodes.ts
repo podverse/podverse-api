70fc154573a1071031556b3377d43fed0996e5bd
suorcd <suorcd@bobot.lol>
2022-12-04 09:19:47 -0800

    Add customOffset

--- src/scripts/episodes/removeDeadEpisodes.ts
+++ src/scripts/episodes/removeDeadEpisodes.ts
@@ -5,13 +5,20 @@ import { connectToDb } from '~/lib/db'
     const customLimit = (process.argv.length > 2 ? process.argv[2] : 100000) as any
     const customLimitInt = parseInt(customLimit, 10)
 
+    const customOffset = (process.argv.length > 2 ? process.argv[3] : 0) as any
+    const customOffset = parseInt(customOffset, 10)
+
     if (!customLimitInt) {
       console.log('customLimit parameter must be an integer.')
       return
     }
 
+    if (!customOffset) {
+      console.log('customOffset parameter must be an integer.')
+      return
+    }
     await connectToDb()
-    await removeDeadEpisodes(customLimitInt)
+    await removeDeadEpisodes(customLimitInt,customOffset)
   } catch (error) {
     console.log(error)
   }
