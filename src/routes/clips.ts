ffe96e0bf8957049c9c449138f749e4b70055714
Mitch Downey <mitchldowney@gmail.com>
2024-04-25 22:35:34 -0500

    Remove clips by episodes.mediaUrl endpoint

--- src/routes/clips.ts
+++ src/routes/clips.ts
@@ -3,27 +3,13 @@ import * as Router from 'koa-router'
 import { config } from '~/config'
 import { emitRouterError } from '~/lib/errors'
 import { convertToChaptersFile } from '~/lib/podcastIndex'
-import { getPublicMediaRefsByEpisodeGuid, getPublicMediaRefsByEpisodeMediaUrl } from '~/controllers/mediaRef'
+import { getPublicMediaRefsByEpisodeGuid } from '~/controllers/mediaRef'
 import { parseNSFWHeader } from '~/middleware/parseNSFWHeader'
 const json = require('koa-json')
 
 const router = new Router({ prefix: `${config.apiPrefix}${config.apiVersion}/clips` })
 
 router.use(bodyParser())
-
-// Get public mediaRefs by episode mediaUrl
-router.get('/', parseNSFWHeader, json(), async (ctx) => {
-  try {
-    const { mediaUrl } = ctx.query
-    const mediaRefsResult = await getPublicMediaRefsByEpisodeMediaUrl(mediaUrl)
-    const mediaRefs = mediaRefsResult[0]
-    const chaptersFile = convertToChaptersFile(mediaRefs)
-    const prettyChaptersFileString = JSON.stringify(chaptersFile, null, 4)
-    ctx.body = JSON.parse(prettyChaptersFileString)
-  } catch (error) {
-    emitRouterError(error, ctx)
-  }
-})
 
 // Get public mediaRefs by episode guid
 router.get('/', parseNSFWHeader, json(), async (ctx) => {
