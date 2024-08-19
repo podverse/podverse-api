00bf9c87c8dc32665128c6cc25c5601b5e4cbbe7
Mitch Downey <mitchldowney@gmail.com>
2024-08-19 01:01:51 -0500

    Rename mediumValuesGetAll

--- src/controllers/mediumValue.ts
+++ src/controllers/mediumValue.ts
@@ -4,7 +4,7 @@ import { MediumValueService } from '@/services/mediumValue';
 const mediumValueService = new MediumValueService();
 
 export class MediumValueController {
-  static async getAllMediumValues(req: Request, res: Response): Promise<void> {
+  static async mediumValuesGetAll(req: Request, res: Response): Promise<void> {
     const result = await mediumValueService.mediumValueGetAll();
     res.json(result);
   }
