f23afd3bfdbdbceb1c4a13b35a68ec7a6837d943
Mitch Downey <mitchldowney@gmail.com>
2025-06-20 23:25:19 -0500

    fix: move dotenvx configuration to non-production environments in index.ts and update package.json dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import './module-alias-config';
-require('@dotenvx/dotenvx').config();
+
+if (process.env.NODE_ENV !== 'production') {
+  require('@dotenvx/dotenvx').config();
+}
 
 import { logger } from 'podverse-helpers';
 import { AppDataSourceRead, AppDataSourceReadWrite } from "podverse-orm";
