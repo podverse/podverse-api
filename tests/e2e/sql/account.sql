7f51920db7880c7398afaf2d7c6a98872d4e7a78
Mitch Downey <mitchldowney@gmail.com>
2025-07-17 14:48:38 -0500

    fix: update hashed passwords for account credentials in SQL seed data

--- tests/e2e/sql/account.sql
+++ tests/e2e/sql/account.sql
@@ -24,7 +24,7 @@ VALUES (
 
 -- Insert into account_credentials table
 INSERT INTO account_credentials (account_id, email, password)
-VALUES (1, 'basic-valid@example.com', '$2b$10$zQl/SdV.Jab8BW47fHCB0.AQK1aZ5AYF.22eCOQVKovKWLViLJIr.');
+VALUES (1, 'basic-valid@example.com', '$2b$10$XmjWYlUZ7bC75XofkIgmsuFG.o5fGbfeIMMzwmsto5wvQdd8CZjLS');
 
 -- Insert into account_email_change_verification table
 INSERT INTO account_email_change_verification (account_id, verification_token, verification_token_expires_at, pending_email_address)
@@ -88,7 +88,7 @@ VALUES (
 
 -- Insert into account_credentials table
 INSERT INTO account_credentials (account_id, email, password)
-VALUES (2, 'trial-valid@example.com', '$2b$10$zQl/SdV.Jab8BW47fHCB0.AQK1aZ5AYF.22eCOQVKovKWLViLJIr.');
+VALUES (2, 'trial-valid@example.com', '$2b$10$XmjWYlUZ7bC75XofkIgmsuFG.o5fGbfeIMMzwmsto5wvQdd8CZjLS');
 
 -- Insert into account_email_change_verification table
 INSERT INTO account_email_change_verification (account_id, verification_token, verification_token_expires_at, pending_email_address)
@@ -152,7 +152,7 @@ VALUES (
 
 -- Insert into account_credentials table
 INSERT INTO account_credentials (account_id, email, password)
-VALUES (3, 'trial-expired@example.com', '$2b$10$zQl/SdV.Jab8BW47fHCB0.AQK1aZ5AYF.22eCOQVKovKWLViLJIr.');
+VALUES (3, 'trial-expired@example.com', '$2b$10$XmjWYlUZ7bC75XofkIgmsuFG.o5fGbfeIMMzwmsto5wvQdd8CZjLS');
 
 -- Insert into account_email_change_verification table
 INSERT INTO account_email_change_verification (account_id, verification_token, verification_token_expires_at, pending_email_address)
@@ -216,7 +216,7 @@ VALUES (
 
 -- Insert into account_credentials table
 INSERT INTO account_credentials (account_id, email, password)
-VALUES (4, 'basic-expired@example.com', '$2b$10$zQl/SdV.Jab8BW47fHCB0.AQK1aZ5AYF.22eCOQVKovKWLViLJIr.');
+VALUES (4, 'basic-expired@example.com', '$2b$10$XmjWYlUZ7bC75XofkIgmsuFG.o5fGbfeIMMzwmsto5wvQdd8CZjLS');
 
 -- Insert into account_email_change_verification table
 INSERT INTO account_email_change_verification (account_id, verification_token, verification_token_expires_at, pending_email_address)
@@ -253,67 +253,3 @@ VALUES (4, 'https://example.com/expired-premium-endpoint', 'expired-premium-publ
 -- Insert into account_verification table
 INSERT INTO account_verification (account_id, verification_token, verification_token_expires_at)
 VALUES (4, 'expired-premium-verification-token', TIMESTAMP '2030-01-01 13:00:00+00');
-
--- admin-valid@example.com
-
--- Insert into account table
-INSERT INTO account (id, id_text, verified, sharable_status_id)
-VALUES (5, 'admin-valid', TRUE, 1);
-
--- Insert into account_app_store_purchase table
-INSERT INTO account_app_store_purchase (
-    account_id, 
-    transaction_id, 
-    product_id, 
-    purchase_date, 
-    expires_date, 
-    is_trial_period
-)
-VALUES (
-    5, 
-    'admin-transaction-id', 
-    'admin-product-id', 
-    TIMESTAMP '2023-01-01 12:00:00+00', 
-    TIMESTAMP '2024-01-01 12:00:00+00', 
-    FALSE
-);
-
--- Insert into account_credentials table
-INSERT INTO account_credentials (account_id, email, password)
-VALUES (5, 'admin-valid@example.com', '$2b$10$hashedpasswordforadminuser');
-
--- Insert into account_email_change_verification table
-INSERT INTO account_email_change_verification (account_id, verification_token, verification_token_expires_at, pending_email_address)
-VALUES (5, 'admin-verification-token', TIMESTAMP '2030-01-01 13:00:00+00', 'admin-valid-new@example.com');
-
--- Insert into account_fcm_device table
-INSERT INTO account_fcm_device (fcm_token, account_id)
-VALUES ('admin-fcm-token', 5);
-
--- Insert into account_google_play_purchase table
-INSERT INTO account_google_play_purchase (account_id, transaction_id, product_id, purchase_token)
-VALUES (5, 'admin-transaction-id', 'admin-product-id', 'admin-purchase-token');
-
--- Insert into account_membership_status table
-INSERT INTO account_membership_status (account_id, account_membership_id, membership_expires_at)
-VALUES (5, 2, TIMESTAMP '2030-01-01 12:00:00+00');
-
--- Insert into account_paypal_order table
-INSERT INTO account_paypal_order (account_id, payment_id, state)
-VALUES (5, 'admin-payment-id', 'completed');
-
--- Insert into account_profile table
-INSERT INTO account_profile (account_id, display_name, bio)
-VALUES (5, 'Admin User', 'This is an admin user.');
-
--- Insert into account_reset_password table
-INSERT INTO account_reset_password (account_id, reset_token, reset_token_expires_at)
-VALUES (5, 'admin-reset-token', TIMESTAMP '2030-01-01 13:00:00+00');
-
--- Insert into account_up_device table
-INSERT INTO account_up_device (account_id, up_endpoint, up_public_key, up_auth_key)
-VALUES (5, 'https://example.com/admin-endpoint', 'admin-public-key', 'admin-auth-key');
-
--- Insert into account_verification table
-INSERT INTO account_verification (account_id, verification_token, verification_token_expires_at)
-VALUES (5, 'admin-verification-token', TIMESTAMP '2030-01-01 13:00:00+00');
