aa799e16dc3e3f8cc739d4af11e2a41cae14247e
Mitch Downey <mitchldowney@gmail.com>
2025-09-21 18:43:11 -0500

    feat: enhance playlist schemas and add methods for top public and subscribed playlists

--- src/controllers/playlist/playlist.ts
+++ src/controllers/playlist/playlist.ts
@@ -1,17 +1,33 @@
 import { NextFunction, Request, Response } from 'express';
 import Joi from 'joi';
-import { SharableStatusEnum } from 'podverse-helpers';
-import { PlaylistService } from 'podverse-orm';
+import { MediumEnum, QUERY_PARAMS_STATS_RANGE_VALUES, QueryParamsPlaylistsSort, QueryParamsPlaylistsSort, QueryParamsStatsRange, SharableStatusEnum } from 'podverse-helpers';
+import { FindManyOptions, Playlist, PlaylistService, StatsAggregatedPlaylist, StatsAggregatedPlaylistService } from 'podverse-orm';
 import { ensureAuthenticated, optionalEnsureAuthenticated } from '@api/lib/auth';
 import { handleGenericErrorResponse } from '../helpers/error';
 import { validateBodyObject, validateParamsObject, validateQueryObject } from '@api/lib/validation';
 import { getPaginationParams } from '../helpers/pagination';
+import { getStatsOrder } from '@api/lib/stats';
+
+type TopPublicPlaylistsParams = {
+  range?: QueryParamsStatsRange;
+  offset?: number;
+  limit?: number;
+  medium_id?: MediumEnum;
+};
+
+type TopSubscribedPlaylistsParams = {
+  playlist_id_texts: string[];
+  range?: QueryParamsStatsRange;
+  offset?: number;
+  limit?: number;
+  medium_id?: MediumEnum;
+};
 
 const playlistSchema = Joi.object({
   title: Joi.string().allow(null, ''),
   description: Joi.string().allow(null, ''),
-  medium: Joi.number().min(1).required(),
-  sharable_status: Joi.number().min(1).required(),
+  medium_id: Joi.number().min(1).required(),
+  sharable_status_id: Joi.number().min(1).required(),
   is_default_favorites: Joi.boolean().required()
 });
 
@@ -19,10 +35,16 @@ const playlistIdSchema = Joi.object({
   playlist_id_text: Joi.string().required()
 });
 
-const paginationSchema = Joi.object({
-  medium: Joi.number().integer().min(1).optional(),
+const getManyPrivateSchema = Joi.object({
+  medium_id: Joi.number().integer().min(1).optional(),
   page: Joi.number().integer().min(1).optional(),
-  limit: Joi.number().integer().min(1).optional()
+});
+
+const getManyPublicSchema = Joi.object({
+  page: Joi.number().integer().min(1).optional(),
+  sort: Joi.string().valid("top").optional(),
+  range: Joi.string().valid(...QUERY_PARAMS_STATS_RANGE_VALUES).optional(),
+  medium_id: Joi.number().integer().min(1).optional()
 });
 
 const playlistService = new PlaylistService();
@@ -78,15 +100,26 @@ const verifyPrivatePlaylistOwnership = () => {
 
 class PlaylistController {
   private static playlistService = new PlaylistService();
+  private static statsAggregatedPlaylistService = new StatsAggregatedPlaylistService();
 
   static async createPlaylist(req: Request, res: Response): Promise<void> {
     ensureAuthenticated(req, res, async () => {
       validateBodyObject(playlistSchema, req, res, async () => {
         const account = req.user!;
-        const dto = req.body;
+
+        const dto = {
+          title: req.body.title,
+          description: req.body.description,
+          medium_id: req.body.medium,
+          sharable_status_id: req.body.sharable_status,
+          is_default_favorites: req.body.is_default_favorites
+        };
 
         try {
-          const playlist = await PlaylistController.playlistService.create(account.id, dto);
+          const playlist = await PlaylistController.playlistService.create(
+            account.id,
+            dto
+          );
           res.status(201).json(playlist);
         } catch (err) {
           handleGenericErrorResponse(res, err);
@@ -102,7 +135,13 @@ class PlaylistController {
           validateBodyObject(playlistSchema, req, res, async () => {
             const account = req.user!;
             const { playlist_id_text } = req.params;
-            const dto = req.body;
+            const dto = {
+              title: req.body.title,
+              description: req.body.description,
+              medium_id: req.body.medium,
+              sharable_status_id: req.body.sharable_status,
+              is_default_favorites: req.body.is_default_favorites
+            };
 
             try {
               const playlist = await PlaylistController.playlistService.update(account.id, playlist_id_text, dto);
@@ -135,20 +174,18 @@ class PlaylistController {
   }
 
   static async getManyPublic(req: Request, res: Response): Promise<void> {
-    validateQueryObject(paginationSchema, req, res, async () => {
+    validateQueryObject(getManyPublicSchema, req, res, async () => {
       try {
-        const { medium } = req.query;
+        const { medium_id, range } = req.query as { medium_id?: MediumEnum; range?: QueryParamsStatsRange };
         const { page, limit, offset } = getPaginationParams(req);
-        const options = {
-          where: {
-            sharable_status: { id: SharableStatusEnum.Public },
-            ...(medium && { medium: { id: medium } })
-          },
-          skip: offset,
-          take: limit,
-          relations: ['account', 'medium']
-        };
-        const playlists = await PlaylistController.playlistService.getMany(options);
+
+        const playlists = await PlaylistController._getTopPublicPlaylists({
+          range,
+          offset,
+          limit,
+          medium_id
+        });
+
         res.status(200).json({
           data: playlists,
           meta: { page }
@@ -161,11 +198,12 @@ class PlaylistController {
 
   static async getManyPrivate(req: Request, res: Response): Promise<void> {
     ensureAuthenticated(req, res, async () => {
-      validateQueryObject(paginationSchema, req, res, async () => {
+      validateQueryObject(getManyPrivateSchema, req, res, async () => {
         try {
           const account = req.user!;
           const { medium } = req.query;
           const { page, limit, offset } = getPaginationParams(req);
+
           const options = {
             where: {
               ...(medium && { medium: { id: medium } }),
@@ -175,7 +213,9 @@ class PlaylistController {
             take: limit,
             relations: ['account', 'medium']
           };
+
           const playlists = await PlaylistController.playlistService.getMany(options);
+          
           res.status(200).json({
             data: playlists,
             meta: { page }
@@ -217,6 +257,29 @@ class PlaylistController {
         });
       });
     });
+  }
+
+  private static async _getTopPublicPlaylists({ range, offset, limit, medium }: TopPublicPlaylistsParams): Promise<Playlist[]> {
+    const order = getStatsOrder(range);
+    const config: FindManyOptions<StatsAggregatedPlaylist> = {
+      order: { [order]: 'DESC' },
+      skip: offset,
+      take: limit
+    };
+
+    const statsResults = await PlaylistController.statsAggregatedPlaylistService.getManyPublic(config, medium);
+    return statsResults.map((stat: { playlist: Playlist }) => stat.playlist).filter(Boolean);
+  }
+
+  private static async _getTopSubscribedPlaylists({ playlist_id_texts, range, offset, limit }: TopSubscribedPlaylistsParams): Promise<Playlist[]> {
+    const order = getStatsOrder(range);
+    const config: FindManyOptions<StatsAggregatedPlaylist> = {
+      order: { [order]: 'DESC' },
+      skip: offset,
+      take: limit
+    };
+    const statsResults = await PlaylistController.statsAggregatedPlaylistService.getManyByPlaylists(playlist_id_texts, config);
+    return statsResults.map((stat: { playlist: Playlist }) => stat.playlist).filter(Boolean);
   }
 }
 
