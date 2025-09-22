9a894e4941893cf1a7b7edba00b1a0cce20f7809
Mitch Downey <mitchldowney@gmail.com>
2025-09-22 12:11:12 -0500

    feat: add private playlist retrieval method to get followed playlists

--- src/lib/subscribed.ts
+++ src/lib/subscribed.ts
@@ -1,7 +1,14 @@
-import { AccountFollowingChannelService } from "podverse-orm";
+import { AccountFollowingChannelService, AccountFollowingPlaylistService } from "podverse-orm";
 
 export async function getFollowedChannelIds(account_id: number): Promise<number[]> {
   const accountFollowingChannelService = new AccountFollowingChannelService();
   const { results } = await accountFollowingChannelService.getFollowedChannelsWithCount(Number(account_id));
   return results.map((f: { channel_id: number }) => f.channel_id);
+}
+
+export async function getFollowedPlaylistIdsPrivate(account_id: number): Promise<number[]> {
+  const accountFollowingPlaylistService = new AccountFollowingPlaylistService();
+  const results = await accountFollowingPlaylistService.getFollowedPlaylistsPrivateWithCount(Number(account_id));
+  const data = results[0];
+  return data.map((f: { playlist_id: number }) => f.playlist_id);
 }
