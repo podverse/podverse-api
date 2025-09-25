9bc3789d06646a14403f21cdf038c70a3e90d891
Mitch Downey <mitchldowney@gmail.com>
2025-09-25 02:07:05 -0500

    feat: implement queueResourceNowPlayingSchema for managing playback details across controllers

--- src/controllers/queue/queueResourceItemAddByRSS.ts
+++ src/controllers/queue/queueResourceItemAddByRSS.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import Joi from 'joi';
-import { addItemToHistorySchema } from './queueResourceItem';
+import { queueResourceNowPlayingSchema } from './queueResourceItem';
 import { QueueResourceService } from 'podverse-orm';
 import { handleGenericErrorResponse } from '@api/controllers/helpers/error';
 import { ensureAuthenticated } from '@api/lib/auth';
@@ -88,13 +88,21 @@ class QueueResourceItemAddByRSSController {
 
   static async addItemAddByRSSToNowPlaying(req: Request, res: Response): Promise<void> {
     validateParamsObject(queueIdSchema, req, res, async () => {
-      ensureAuthenticated(req, res, async () => {
-        verifyQueueOwnership()(req, res, async () => {
-          validateBodyObject(addItemToQueueSchema, req, res, async () => {
+      validateBodyObject(queueResourceNowPlayingSchema, req, res, async () => {
+        ensureAuthenticated(req, res, async () => {
+          verifyQueueOwnership()(req, res, async () => {
             const { queue_id_text } = req.params;
-            const { add_by_rss_resource_data } = req.body;
+            const { add_by_rss_resource_data, playback_position, media_file_duration, completed } = req.body;
+
+            const dto = {
+              add_by_rss_resource_data,
+              ...((playback_position || playback_position === 0) ? { playback_position } : {}),
+              ...((media_file_duration || media_file_duration === 0) ? { media_file_duration } : {}),
+              ...(completed ? { completed } : {})
+            };
+
             try {
-              const queueResource = await QueueResourceItemAddByRSSController.queueResourceService.addItemAddByRSSToNowPlaying(queue_id_text, add_by_rss_resource_data);
+              const queueResource = await QueueResourceItemAddByRSSController.queueResourceService.addItemAddByRSSToNowPlaying(queue_id_text, dto);
               res.status(201).json(queueResource);
             } catch (err) {
               handleGenericErrorResponse(res, err);
@@ -107,7 +115,7 @@ class QueueResourceItemAddByRSSController {
 
   static async addItemAddByRSSToHistory(req: Request, res: Response): Promise<void> {
     validateParamsObject(queueIdSchema, req, res, async () => {
-      validateBodyObject(addItemToHistorySchema, req, res, async () => {
+      validateBodyObject(queueResourceNowPlayingSchema, req, res, async () => {
         ensureAuthenticated(req, res, async () => {
           verifyQueueOwnership()(req, res, async () => {
             const { queue_id_text } = req.params;
