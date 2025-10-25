d1a26ccab9494b8dcf1512835f09704c3f93a422
Mitch Downey <mitchldowney@gmail.com>
2025-10-24 20:25:38 -0500

    feat: add history resources endpoint with pagination to queue resource controller

--- src/controllers/queue/queueResource.ts
+++ src/controllers/queue/queueResource.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from 'express';
 import Joi from 'joi';
-import { QueueResourceService } from 'podverse-orm';
+import { QueueResource, QueueResourceService } from 'podverse-orm';
 import { handleGenericErrorResponse } from '../helpers/error';
 import { ensureAuthenticated } from '@api/lib/auth';
 import { verifyQueueOwnership } from '@api/controllers/queue/queue';
 import { validateParamsObject } from '@api/lib/validation';
+import { getPaginationParams } from '../helpers/pagination';
+import { ApiListResponse } from 'podverse-helpers';
 
 const queueIdSchema = Joi.object({
   queue_id_text: Joi.string().required()
@@ -62,6 +64,35 @@ class QueueResourceController {
               .queueResourceService
               .getAllUpcomingByQueueIdText(queue_id_text);
             res.status(200).json(queueResources);
+          } catch (err) {
+            handleGenericErrorResponse(res, err);
+          }
+        });
+      });
+    });
+  }
+
+  static async getHistoryResourcesByQueueIdText(req: Request, res: Response): Promise<void> {
+    validateParamsObject(queueIdSchema, req, res, async () => {
+      ensureAuthenticated(req, res, async () => {
+        verifyQueueOwnership()(req, res, async () => {
+          const { queue_id_text } = req.params;
+          const { page, limit, offset } = getPaginationParams(req);
+
+          try {
+            const queueResources = await QueueResourceController
+              .queueResourceService
+              .getHistoryResourcesByQueueIdText(queue_id_text, {
+                skip: offset,
+                take: limit
+              });
+
+            const response: ApiListResponse<QueueResource> = {
+              data: queueResources[0],
+              meta: { page, count: queueResources[1], limit }
+            };
+
+            res.status(200).json(response);
           } catch (err) {
             handleGenericErrorResponse(res, err);
           }
--- src/routes/queue.ts
+++ src/routes/queue.ts
@@ -15,6 +15,7 @@ router.get('/all-for-account/private', asyncHandler(QueueController.getAllPrivat
 router.get('/:queue_id_text/resources', asyncHandler(QueueResourceController.getAllByQueueIdTextPrivate));
 router.get('/:queue_id_text/resources/now-playing', asyncHandler(QueueResourceController.getNowPlayingByQueueIdText));
 router.get('/:queue_id_text/resources/upcoming-all', asyncHandler(QueueResourceController.getAllUpcomingByQueueIdText));
+router.get('/:queue_id_text/resources/history-paginated', asyncHandler(QueueResourceController.getHistoryResourcesByQueueIdText));
 router.post('/:queue_id_text/update-is-active', asyncHandler(QueueController.updateIsActiveQueue));
 
 router.post('/:queue_id_text/clip/:clip_id_text/now-playing', asyncHandler(QueueResourceClipController.addClipToNowPlaying));
