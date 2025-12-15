91220b851757be76261b76d63d92c7b604809db2
Mitch Downey <mitchldowney@gmail.com>
2025-12-14 21:06:39 -0600

    fix: enhance rssAddToOnDemandMQ to include onDemandParserEvent type handling

--- src/controllers/mq/mq.ts
+++ src/controllers/mq/mq.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import Joi from "joi";
-import { MQ_QUEUES } from "podverse-helpers";
+import { MQ_QUEUES, OnDemandParserEventType } from "podverse-helpers";
 import { mqRSSAdd } from "podverse-mq";
 import { ensureAuthenticated } from "@api/lib/auth";
 import { validateBodyObject } from "@api/lib/validation";
@@ -16,36 +16,45 @@ const addToOnDemandMQSchema = Joi.object({
 export class MQController {
   static rssOnDemandMiddleware = rateLimitAuthEndpoint({
     windowMs: 60 * 60 * 1000,
-    max: 10
+    max: 20
   });
 
-  static async rssAddToOnDemandMQ(req: Request, res: Response): Promise<void> {
-    ensureAuthenticated(req, res, async () => {
-      MQController.rssOnDemandMiddleware(req, res, () => {
-        validateBodyObject(addToOnDemandMQSchema, req, res, async () => {
-          const dto = req.body;
-          const finalDto = {
-            url: dto.url,
-            podcast_index_id: dto.podcast_index_id
-          };
+  static rssAddToOnDemandMQ(type: OnDemandParserEventType) {
+    return async (req: Request, res: Response): Promise<void> => {
+      ensureAuthenticated(req, res, async () => {
+        MQController.rssOnDemandMiddleware(req, res, () => {
+          validateBodyObject(addToOnDemandMQSchema, req, res, async () => {
+            const dto = req.body;
+            const finalDto = {
+              url: dto.url,
+              podcast_index_id: dto.podcast_index_id,
+            };
 
-          try {
-            const mqConstantMessageOptions = MQ_QUEUES['rss-on-demand'];
-            await mqRSSAdd(
-              activeMQArtemisService,
-              {
-                ...mqConstantMessageOptions,
-                feedUrl: finalDto.url,
-                podcast_index_id: finalDto.podcast_index_id,
-              },
-              { forceParse: false }
-            );
-            res.status(201).json({ message: "Feed added to on-demand queue successfully." });
-          } catch (err) {
-            handleGenericErrorResponse(res, err);
-          }
+            try {
+              const mqConstantMessageOptions = MQ_QUEUES['rss-on-demand'];
+              await mqRSSAdd(
+                activeMQArtemisService,
+                {
+                  ...mqConstantMessageOptions,
+                  feedUrl: finalDto.url,
+                  podcast_index_id: finalDto.podcast_index_id,
+                },
+                {
+                  forceParse: false,
+                  onDemandParserEvent: {
+                    accountId: req.user!.id,
+                    remoteParentPodcastIndexId: null,
+                    type: type,
+                  },
+                },
+              );
+              res.status(201).json({ message: 'Feed added to on-demand queue successfully.' });
+            } catch (err) {
+              handleGenericErrorResponse(res, err);
+            }
+          });
         });
       });
-    });
+    };
   }
 }
--- src/routes/mq.ts
+++ src/routes/mq.ts
@@ -1,3 +1,4 @@
+import { OnDemandParserEventType } from 'podverse-helpers';
 import { Router } from 'express';
 import { config } from '@api/config';
 import { MQController } from '@api/controllers/mq/mq';
@@ -7,6 +8,8 @@ const router = Router();
 
 router.use(`${config.api.prefix}${config.api.version}/mq`, router);
 
-router.post('/rss/add/on-demand', asyncHandler(MQController.rssAddToOnDemandMQ));
+router.post('/rss/add/on-demand', asyncHandler(MQController.rssAddToOnDemandMQ(OnDemandParserEventType.ADD)));
+
+router.post('/rss/refresh/on-demand', asyncHandler(MQController.rssAddToOnDemandMQ(OnDemandParserEventType.REFRESH)));
 
 export const mqRouter = router;
